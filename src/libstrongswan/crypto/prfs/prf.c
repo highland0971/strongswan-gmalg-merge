13cf8272c8fb689512e5ba24d498a0d95af6af64
highland <joy.highland@139.com>
2021-06-16 19:45:10 +0800

    fix oid_hmac_sm3 missing error

--- src/libstrongswan/crypto/prfs/prf.c
+++ src/libstrongswan/crypto/prfs/prf.c
@@ -53,7 +53,7 @@ pseudo_random_function_t pseudo_random_function_from_oid(int oid)
 			return PRF_HMAC_SHA2_384;
 		case OID_HMAC_SHA512:
 			return PRF_HMAC_SHA2_512;
-		case OID_HMAC_SM3:
+		case OID_SM3:
 			return PRF_HMAC_SM3;
 		case OID_HMAC_SHA224:
 		case OID_HMAC_SHA512_224:
