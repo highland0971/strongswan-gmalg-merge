cc23a59279336e799f35d5583780b8ee3536718f
highland <joy.highland@139.com>
2021-06-16 19:13:38 +0800

    fix ENUM_NEXT for sm1\sm4  compile error

--- src/libstrongswan/crypto/crypters/crypter.h
+++ src/libstrongswan/crypto/crypters/crypter.h
@@ -63,6 +63,12 @@ enum encryption_algorithm_t {
 	// ENCR_SM4_ECB            = 30,
 	// ENCR_SM4_CBC            = 31,
 
+	//Added by according to GB/T 36968-2018
+	ENCR_SM1_CBC            = 128,
+	ENCR_SM4_CBC            = 129,
+
+	ENCR_SM1_ECB            = 130,
+	ENCR_SM4_ECB            = 131,
 	
 	/** Orgin version
 	* ENCR_CHACHA20_POLY1305 =  28,
@@ -70,12 +76,7 @@ enum encryption_algorithm_t {
 	// ENCR_CHACHA20_POLY1305 =  32,
 	ENCR_CHACHA20_POLY1305 =  28,
 
-	//Added by according to GB/T 36968-2018
-	ENCR_SM1_CBC            = 128,
-	ENCR_SM4_CBC            = 129,
 
-	ENCR_SM1_ECB            = 130,
-	ENCR_SM4_ECB            = 131,
 
 
 	ENCR_UNDEFINED =        1024,
